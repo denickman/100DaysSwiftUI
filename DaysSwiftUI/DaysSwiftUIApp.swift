b9b2c9cbf6d92b451dd8d3d6f4347cbbe5bcd52e
Denis Yaremenko <yaremdennis@gmail.com>
2021-05-19 10:17:57 +0300

    Initial Commit

---
+++ DaysSwiftUI/DaysSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaysSwiftUIApp.swift
+//  DaysSwiftUI
+//
+//  Created by Denis Yaremenko on 5/19/21.
+//
+
+import SwiftUI
+
+@main
+struct DaysSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
